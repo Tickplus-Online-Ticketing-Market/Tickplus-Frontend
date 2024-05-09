a758896a12564411319987b5fbf9ee11e4edb465
YasiraNBD <155614875+ynbdissanayake@users.noreply.github.com>
2024-05-09 18:38:40 +0530

    Payment Update

--- src/Components/TicketPurchase/FinanceStatus.jsx
+++ src/Components/TicketPurchase/FinanceStatus.jsx
@@ -19,38 +19,6 @@ useEffect(() => {
     <div style={{ transform: loading ? 'translateY(100%)' : 'translateY(0)', transition: 'transform 0.5s ease-in-out' }}>
       <div className='relative'>
       <div className=''>
-        <span className='text-3xl bg-background text-primary flex justify-center mt-[2rem] mb-[2rem] rounded-lg'>Bank Transfers Confirmations</span>
-        <div className="bg-accent relative overflow-x-auto shadow-md sm:rounded-lg px-5 py-5 mb-10">
-          <table className="w-full text-xl text-center rtl:text-right text-primary dark:text-primary">
-            <thead className="text-xl text-primary uppercase bg-accent dark:text-white">
-              <tr>
-                <th scope="col" className="px-6 py-3">
-                  Event ID
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Transaction ID
-                </th>
-                
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="bg-text">
-                <th scope="row" className="px-6 py-4 font-xl text-background">
-                  HK458452
-                </th>
-                <td className="px-6 py-3 text-background">
-                  TID5685
-                </td>
-                <td className="px-6 py-4 text-primary items-center">
-                  <button type="button"  className='bg-primary text-background h-[3rem] w-[5rem] rounded hover:scale-95 transition text-xl mr-1'>View Slip</button>
-                </td>
-              </tr>
-              
-            </tbody>
-          </table>
-        </div>
-
-      {/*Table 02 */}
         <span className='text-3xl bg-background text-primary flex justify-center mt-[5rem] mb-[2rem]'>Refund Requests</span>
         <div className="bg-accent relative overflow-x-auto shadow-md sm:rounded-lg px-5 py-5">
           <table className="w-full text-xl text-center rtl:text-right text-primary dark:text-primary">
--- src/Components/TicketPurchase/models/BuyModel.jsx
+++ src/Components/TicketPurchase/models/BuyModel.jsx
@@ -1,28 +1,14 @@
-import React, { useState } from 'react';
-import CardPayments from './CardPayments';
-import BankTransfers from './BankTransfers';
+import React from 'react';
 
 export default function BuyModel({ visible, onClose }) {
-//Fetch Wishes
-
-    const [showMyModel06, setShowMyModel06] = useState(false);
-    const [showMyModel07, setShowMyModel07] = useState(false);
 
     const handleOnClose01 = (e) => {
         if (e.target.id === 'container') onClose();
     };
 
-    if (!visible) return null;
+    if(!visible) return null;
 
-    const handleCardPaymentsClick = () => {
-        setShowMyModel06(true);
-        setShowMyModel07(false); 
-    };
 
-    const handleBankTransfersClick = () => {
-        setShowMyModel07(true);
-        setShowMyModel06(false); 
-    };
 
     return (
         <div
@@ -36,21 +22,10 @@ export default function BuyModel({ visible, onClose }) {
                     <img src="./images/tick+1.png" alt=""/>
                 </div> 
 
-                <div>
-                    <span className='flex justify-center pr-[5rem] mb-10 text-accent text-2xl'>Select Payment Method</span>
                 
-                    <div className='flex justify-center pr-[5rem]'>
-                        <button type="button" onClick={handleCardPaymentsClick} className="bg-primary text-background h-[3rem] w-[15rem] rounded hover:scale-95 transition text-xl mb-5">Card Payments</button> 
-                    </div>
-
-                    <div className='flex justify-center pr-[5rem]'>
-                        <button type="button" onClick={handleBankTransfersClick} className="bg-primary text-background h-[3rem] w-[15rem] rounded hover:scale-95 transition text-xl">Bank Transfers</button>
-                    </div>                    
-                </div>
             </div>
 
-            {showMyModel06 && <CardPayments onClose={() => setShowMyModel06(false)} visible={true} />}
-            {showMyModel07 && <BankTransfers onClose={() => setShowMyModel07(false)} visible={true} />}
+            
         </div>
     );
 }
